cd914f35d0f934a64e6511b2ae4f7c61545a8f89
abdullahshehzad63 <abdullahshehzad63@gmail.com>
2023-12-06 12:02:31 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log('hey our js file is including here');
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+menu.onclick = ()=>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
